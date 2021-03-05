13f53a7b041c1fe9c9c5d4415af7c085b40b50a8
Danny Oh <dannyoh0316@gmail.com>
2021-03-04 20:42:01 -0600

    finished news import ETL

--- news-import/src/main/java/edu/northwestern/ssa/App.java
+++ news-import/src/main/java/edu/northwestern/ssa/App.java
@@ -1,7 +1,10 @@
 package edu.northwestern.ssa;
 
 import org.archive.io.ArchiveReader;
+import org.archive.io.ArchiveRecord;
 import org.archive.io.warc.WARCReaderFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.http.HttpExecuteResponse;
@@ -12,9 +15,11 @@ import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.List;
 import java.util.Scanner;
@@ -75,6 +80,64 @@ public class App {
         HttpExecuteResponse esIndexResponse = es.createIndex();
         esIndexResponse.responseBody().get().close();
 
+        int count = 0;
+
+        // Iterate through the ArchiveRecord objects.
+        for (ArchiveRecord record : reader) {
+            // Get the WARC type.
+            Object warcType = record.getHeader().getHeaderValue("WARC-Type");
+
+            if (warcType.equals("response")) {
+                // Use the read() method to iterate through the record.
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                int nRead;
+                byte[] data = new byte[1024];
+                while ((nRead = record.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, nRead);
+                }
+                String contents = buffer.toString(String.valueOf(StandardCharsets.UTF_8));
+
+                // The first occurrence of "\r\n\r\n" (blank new line) marks the transition to HTML.
+                int firstIndex = contents.indexOf("\r\n\r\n");
+
+                // Get the HTML.
+                String html = contents.substring(firstIndex + 4);
+
+                /*
+                Parse the HTML and post the <url, title, txt> to Elasticsearch if in English.
+                 */
+
+                try {
+                    // Create a Document object from the HTML string extracted above.
+                    Document doc = Jsoup.parse(html);
+
+                    // Get the language.
+                    String lang = getLang(doc);
+
+                    if (lang.equals("en")) {
+                        // Extract the document title.
+                        String title = getTitle(doc);
+
+                        // Extract the plain text.
+                        String txt = getTxt(doc);
+
+                        // Get the URL of the page.
+                        String url = record.getHeader().getUrl();
+
+                        // Post the document to the Elasticsearch index.
+                        postDocument(es, title, txt, url);
+
+                        count++;
+                        if (count % 100 == 0) {
+                            System.out.println(count);
+                        }
+                    }
+                } catch (Exception e) {
+                    // Ignore the exception.
+                }
+            }
+        }
+
         // Close the ElasticSearch object.
         es.close();
 
@@ -94,7 +157,7 @@ public class App {
             } else {
                 String lastCrawl = readLastCrawl(latestCrawl);
                 if (lastCrawl.equals(latestKey)) {
-                    System.out.println("No new crawl data");
+                    System.out.println("No new crawl data.");
                     return false;
                 } else {
                     System.out.println("Getting new crawl data.");
@@ -129,5 +192,45 @@ public class App {
         } catch (Exception e) {
             return "";
         }
+    }
+
+    private static String getLang(Document doc) {
+        try {
+            return doc.select("html").attr("lang");
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    private static String getTitle(Document doc) {
+        try {
+            return doc.title();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    private static String getTxt(Document doc) {
+        try {
+            return doc.text();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    private static void postDocument(ElasticSearch es, String title, String txt, String url) throws IOException {
+        HttpExecuteResponse postDocResponse;
+
+        // Keep trying to post document until it succeeds.
+        while (true) {
+            try {
+                postDocResponse = es.postDoc(title, txt, url);
+                break;
+            } catch (Exception e) {
+                // Ignore the exception.
+            }
+        }
+
+        postDocResponse.responseBody().get().close();
     }
 }
